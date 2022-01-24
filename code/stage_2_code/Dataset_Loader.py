f8f312c57aefafcb3789b93bd6fb180d8c822dbe
Careless-Whisper999 <houcao@ucdavis.edu>
2022-01-24 10:58:30 -0800

    added files for stage 2 and implemented dataset loader

---
+++ code/stage_2_code/Dataset_Loader.py
@@ -0,0 +1,30 @@
+'''
+Concrete IO class for a specific dataset
+'''
+
+# Copyright (c) 2017-Current Jiawei Zhang <jiawei@ifmlab.org>
+# License: TBD
+import csv
+from code.base_class.dataset import dataset
+
+
+class Dataset_Loader(dataset):
+    data = None
+    dataset_source_folder_path = None
+    dataset_source_file_name = None
+    
+    def __init__(self, dName=None, dDescription=None):
+        super().__init__(dName, dDescription)
+    
+    def load(self):
+        print('loading data...')
+        X = []
+        y = []
+        with open(self.dataset_source_folder_path + self.dataset_source_file_name, 'r') as f:
+            reader = csv.reader(f)
+            for line in reader:
+                elements = [int(i) for i in line]
+                X.append(elements[1:])
+                y.append(elements[0])
+        f.close()
+        return {'X': X, 'y': y}
